8314fc9d6bb970117fc1aa35a154434df817e590
KavindhyaJay <kavindhyauj@gmail.com>
2026-09-25 02:31:31 +0530

    Fix GitHub Pages routing

--- web-app/src/main.jsx
+++ web-app/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { PredictionProvider } from './context/PredictionContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <PredictionProvider>
         <App />
       </PredictionProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
