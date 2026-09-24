8e4187d14ef204742a2da46558d28091754bcd67
KavindhyaJay <kavindhyauj@gmail.com>
2026-09-25 01:03:25 +0530

    Deploy frontend to GitHub Pages

--- web-app/vite.config.js
+++ web-app/vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Tea-Yield-Predictor/',
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
 })
